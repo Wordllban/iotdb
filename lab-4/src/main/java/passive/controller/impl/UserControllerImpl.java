61becfa57bd12f93856525f503c978b27722680b
KMininkov <mininkov3228@gmail.com>
2021-10-14 13:25:56 +0300

    Implemented console view

--- lab-4/src/main/java/passive/controller/impl/UserControllerImpl.java
+++ lab-4/src/main/java/passive/controller/impl/UserControllerImpl.java
@@ -1,7 +1,6 @@
 package passive.controller.impl;
 
 import passive.controller.Controller;
-import passive.model.entities.HotelRoomTypeEntity;
 import passive.model.entities.UserEntity;
 import passive.model.service.impl.UserServiceImpl;
 
---
+++ lab-4/src/main/java/passive/view/Printable.java
@@ -0,0 +1,6 @@
+package passive.view;
+
+@FunctionalInterface
+public interface Printable {
+  void print();
+}
